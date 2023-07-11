427fc6d1226fd8dc2c3e88dc8e9ed3aea08ead3c
Jennifer Santos <jennylima10@gmail.com>
2023-07-11 16:30:35 -0300

    Fazendo diferenciação por cores e quantidade de palavras

--- most-used-words/src/components/PillComponent.vue
+++ most-used-words/src/components/PillComponent.vue
@@ -1,13 +1,32 @@
 <template>
-    <div class="pill">
+    <div class="pill" :style="{backgroundColor:amountColor}">
         <span class="word">{{ word }}</span>
-        <span class="amount">{{ amount }}</span>
+        <span class="amount" :style="{color:amountColor}">{{ amount }}</span>
     </div>
 </template>
 
 <script>
 export default {
-    props: ["word", "amount"]
+    props: ["word", "amount"],
+    computed:{
+        amountColor(){
+            if(this.amount > 1000){
+                return "#ff061a";
+            } else if (this.amount > 700){
+                return "#ff6221"
+            } else if (this.amount > 500){
+                return "#ff9e31"
+            } else if (this.amount > 250){
+                return "#f2d040"
+            } else if (this.amount > 100){
+                return "#00d13c"
+            } else if (this.amount > 50){
+                return "#0073c3"
+            } else {
+                return "#9175a1"
+            }
+        }
+    }
 }
 
 </script>
@@ -22,8 +41,7 @@ export default {
     font-size: 1.3rem;
     font-weight: bold;
     display: flex;
-    align-items: center;
-    background-color: red;
+    align-items: center;    
 }
 
 .pill .word {
@@ -35,7 +53,6 @@ export default {
     height: 40px;
     border-radius: 50%;
     background-color: #ffffff;
-    color: red;
     margin: 10px;
     padding: 5px;
     font-size:14px;
