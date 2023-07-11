76a3bb307bc1da34bf483f5b6af0d61eb0d14426
Jennifer Santos <jennylima10@gmail.com>
2023-07-11 14:44:16 -0300

    Criação de componentes

--- most-used-words/src/App.vue
+++ most-used-words/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-app-bar app color="primary">
+        <v-toolbar-title>Most Used Words</v-toolbar-title>
+      </v-app-bar>
+      <HomePage/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from "./components/HomePage"
 
 export default {
   name: 'App',
-
+  
   components: {
-    HelloWorld,
+    HomePage
   },
 
   data: () => ({
---
+++ most-used-words/src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+        <div class="pills">
+            <PillComponent v-for="item in groupedWords" :word="item.word"
+            :amount="item.amount" :key="item.word"/>
+        </div>  
+</template>
+
+<script>
+import PillComponent from './PillComponent.vue';
+export default {
+    components: {
+        PillComponent
+    },
+    data: function () {
+        return {
+            groupeWords: [
+                {word: "i", amount:567},
+                {word: "you", amount: 478},
+                {word: "it", amount: 10}
+            ]
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+.pills{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
